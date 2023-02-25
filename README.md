c0c72ff0c858e3ea7fc6407d8483f3a5f44bd8fe
Jason Hartgraves <jharttech@hotmail.com>
2023-02-24 20:08:55 -0600

    Initial upload to main branch.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # prep_chromebook_for_linux_install
-Use Centipede to help prep chromebook for linux install.
+<p>Use Centipede to help prep chromebook for linux install.<br>
+Currently written with Linux workstation in mind.</p>
+<h1><b>Usage</b></h1>
+<li>Install Arduino IDE
+<ul><a href="https://www.arduino.cc/en/Guide/Linux">Arduino IDE webpage</a></ul>
+<li>Copy the script.ino file, in this repo, into the IDE
+<li>Connect the centipede to your machine.
+<li>Upload the code onto the centipede.
+<li>Put chromebook in to developer mode.
+<li>Once into developer mode, access the terminal.
+<li>Log in as chronos user in terminal <code>$: chronos</code>
+<li>Escalate using <code>$: sudo su</code>
+<li>Plug centipede into chromebook and let it run its commands.
+
+<h1><b>Notes:</b></h1>
+<p>Currently if you change the void loop() to an int main() it will lock up the centipede.  I have not yet discovered if it is hard bricked locked up.</P>
+
+<h2><b>Issues</b></h2>
+<li>The Keyboard.print(), Keyboard.println(), Keyboard.press() functions sometimes do not return the full string or list of characters. Still looking at why.
---
+++ scirpt.ino
@@ -0,0 +1,108 @@
+/*
+DO NOT CHANGE void loop() to an int main() or it will lock up the Arduion centipede.
+For use on chromebook in developer mode escalated as root.
+*/
+
+#include <Keyboard.h> // Include the virtual keyboard
+
+#define slowMode 1 // [0,1] Set to 1 if Centipede appears to be moving too quickly at any screen. This will slow down the entire process
+// Special Characters Definition
+#define KEY_ENTER     0xB0
+
+int RXLED = 17;
+static uint8_t __clock_prescaler = (CLKPR & (_BV(CLKPS0) | _BV(CLKPS1) | _BV(CLKPS2) | _BV(CLKPS3)));
+bool running = true;
+
+void setup() {
+  setPrescaler(); //Set prescaler to highest clock speed
+  Keyboard.begin(); //Initiate the keyboard emulator
+  wait(5); //Wait for all services to finish loading
+}
+
+void loop() {
+  if (running) { //Using this boolean to stop the loop from restarting
+    // Add more command functions towards bottom or program and call them from this function
+    wait(5)
+    enterCommandOne(); // Call function
+    wait(5);
+    enterCommandTwo(); // Call function
+    wait(5);
+    enterCommandThree(); // Call function
+    wait(5);
+  };
+  running = false;
+}
+
+// Using this function to compare performace between Keyboard.press() using the string like a list of characters
+// and using the Keyboard.print() function
+// Uncomment Keyboard.print(command_one); to change back.
+void enterCommandOne() {
+  static String command_one = "flashrom --wp-status"; // Setting as static to help load into memory sooner
+  wait(2);
+  //Comment out the next 4 lines to turn off Keyboard.press()
+  for(int i = 0; i < command_one.length(); i++) {
+    Keyboard.press(command_one[i]);
+    Keyboard.releaseAll();
+  }
+  // Uncomment to change function back
+  //Keyboard.print(command_one); //Print string
+  wait(5);
+  Keyboard.write(KEY_ENTER); // Press Enter
+}
+
+void enterCommandTwo() {
+  static String command_two = "flashrom --wp-disable"; // Setting as static to help load into memory sooner
+  wait(2);
+  Keyboard.print(command_two);
+  wait(5);
+  Keyboard.write(KEY_ENTER); // Press Enter
+}
+
+void enterCommandThree() {
+  static String command_one = "flashrom --wp-status"; // Setting as static to help load into memory sooner
+  wait(2);
+  // Using following line to compare performance of Keyboard.println() vs Keyboard.print()
+  // Uncomment the following line to change to Keyboard.println()
+  //Keyboard.println(command_three);
+  // Comment to following to turn off Keyboard.print()
+  Keyboard.print(command_three)
+  wait(5);
+  Keyboard.write(KEY_ENTER); // Press Enter
+}
+
+void setPrescaler() {
+    //Disable interrupts.
+  uint8_t oldSREG = SREG;
+  cli();
+
+  // Enable change.
+  CLKPR = _BV(CLKPCE); // write the CLKPCE bit to one and all the other to zero
+
+  // Change clock division.
+  CLKPR = 0x0; // write the CLKPS0..3 bits while writing the CLKPE bit to zero
+
+  // Copy for fast access.
+  __clock_prescaler = 0x0;
+
+  // Recopy interrupt register.
+  SREG = oldSREG;
+}
+
+void blink() {
+  digitalWrite(RXLED, LOW);
+  //  TXLED1;
+  delay(250);
+  digitalWrite(RXLED, HIGH);
+  //  TXLED0;
+  delay(250);
+}
+
+
+// Defines wait()
+void wait(int cycles) {
+  for (int i = 0; i < cycles; i++) {
+    blink();
+    if (slowMode) {
+      delay(250);
+  }
+}
