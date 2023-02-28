aecf478f4d09fbd1d4b416bc5ee2935e8b8d4661
Jason Hartgraves <jharttech@hotmail.com>
2023-02-27 18:15:27 -0600

    Finishing condensing of code and comments now that testing of code works.

--- scirpt.ino
+++ scirpt.ino
@@ -9,6 +9,10 @@ For use on chromebook in developer mode escalated as root.
 // Special Characters Definition
 #define KEY_ENTER     0xB0
 
+//Add your command variables below
+Static string command_one = "flashrom --wp-status";
+Static string command_two = "flashrom --wp-disable";
+
 int RXLED = 17;
 static uint8_t __clock_prescaler = (CLKPR & (_BV(CLKPS0) | _BV(CLKPS1) | _BV(CLKPS2) | _BV(CLKPS3)));
 bool running = true;
@@ -21,45 +25,21 @@ void setup() {
 
 void loop() {
   if (running) { //Using this boolean to stop the loop from restarting
-    // Add more command functions towards bottom or program and call them from this function
-    enterCommandOne(); // Call function
-    enterCommandTwo(); // Call function
-    enterCommandThree(); // Call function
+    // Add more runCommand function calls with your variable (added above) as a parameter 
+    runCommand(command_one);
+    runCommand(command_two);
+    runCommand(command_one);
   };
   running = false;
 }
 
-// Using this function to compare performace between Keyboard.press() using the string like a list of characters
-// and using the Keyboard.print() function
-void enterCommandOne() {
-  static String command_one = "flashrom --wp-status"; // Setting as static to help load into memory sooner
-  //Uncomment the next 4 lines to try Keyboard.press()
-  for(int i = 0; i < command_one.length(); i++) {
-    delay(50);
-    Keyboard.press(command_one[i]);
+void runCommand(command){
+  for int(i=0; i < command.length(); i++){
+    delay(50)
+    Keyboard.press(command[i])
     Keyboard.releaseAll();
   }
-  Keyboard.write(KEY_ENTER); // Press Enter
-}
-
-void enterCommandTwo() {
-  static String command_two = "flashrom --wp-disable"; // Setting as static to help load into memory sooner
-  for(int i = 0; i < command_two.length(); i++) {
-    delay(50);
-    Keyboard.press(command_two[i]);
-    Keyboard.releaseAll();
-  }
-  Keyboard.write(KEY_ENTER); // Press Enter
-}
-
-void enterCommandThree() {
-  static String command_three = "flashrom --wp-status"; // Setting as static to help load into memory sooner
-  for(int i = 0; i < command_three.length(); i++) {
-    delay(50);
-    Keyboard.press(command_three[i]);
-    Keyboard.releaseAll();
-  }
-  Keyboard.write(KEY_ENTER); // Press Enter
+  Keyboard.write(KEY_ENTER);
 }
 
 void setPrescaler() {
